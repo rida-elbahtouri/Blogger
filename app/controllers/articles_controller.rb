8bdf68fabb23d414c494efbdcb035cdb494d0661
8Bts <mcmizze@yahoo.com>
2020-07-17 13:00:02 +0400

    Add create action for ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,29 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
 	end
 
 	def show
 		@article = Article.find(params[:id])
-	end
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
